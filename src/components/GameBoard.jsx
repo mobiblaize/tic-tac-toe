f558e4b80eda44df605c388c741bcc1c719d8c1f
Emmanuel Asimobi <asimobiemmanuel@gmail.com>
2025-06-21 17:11:06 +0100

    TIC TAC TOE Completed

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,25 @@
+export default function GameBoard({ onSelectSquare, board }) {
+    // const [ gameBoard, setGameBoard ] = useState(initialGameBoard);
+
+    // function handleSelectSquare(rowIndex, colIndex) {
+    //     setGameBoard((prevGameBoard) => {
+    //         let updatedBoard = [...prevGameBoard.map(innerArray=> [...innerArray])];
+    //         updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+    //         return updatedBoard;
+    //     });
+
+    //     onSelectSquare();
+    // }
+
+    return <ol id="game-board">
+        {board.map((row, rowIndex)=> 
+        <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => <li key={colIndex}>
+                    <button disabled={!!playerSymbol} onClick={() => onSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                </li>)}
+            </ol>
+        </li>
+        )}
+    </ol>
+}
